b738313087efb9741eaa09eca3f6315e7d556804
stevenbeers <stevenbeers@mac.com>
2022-05-03 13:17:16 -0400

    Removing to date conversions for aging wip and run dataframes.

--- ChartBuilderClass.py
+++ ChartBuilderClass.py
@@ -91,12 +91,12 @@ class ChartBuilderClass:
 
 	def get_aging_wip_df(self):  # pragma: no cover
 		return_df = self.aging_wip_df.copy()  # pragma: no cover
-		return_df['Done_Date'] = return_df['Done_Date'].dt.date  # pragma: no cover
+		# return_df['Done_Date'] = return_df['Done_Date'].dt.date  # pragma: no cover
 		return return_df  # pragma: no cover
 
 	def get_run_df(self):  # pragma: no cover
 		return_df = self.run_df.copy()  # pragma: no cover
-		return_df['Date'] = return_df['Date'].dt.date  # pragma: no cover
+		# return_df['Date'] = return_df['Date'].dt.date  # pragma: no cover
 		return return_df  # pragma: no cover
 
 	def get_throughput_hist_df(self):  # pragma: no cover
